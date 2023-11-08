39040f3997c0dca8fcff6a59a1156caa3ff2e1b5
Shaxzod Allaberganov <149754223+LIDERCODERuz@users.noreply.github.com>
2023-11-08 07:40:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 value = ''
-old_value = ''
+value2 = ''
 
 @dp.message_handler(commands=["start"], state='*')
 async def start_page(message: types.Message):
@@ -18,7 +18,7 @@ async def start_page(message: types.Message):
 
 @dp.callback_query_handler()
 async def math_func(query):
-    global value, old_value
+    global value, value2
     data = query.data
 
     if data == 'no':
@@ -35,12 +35,12 @@ async def math_func(query):
             value = "⛔️ Tizimda xatolik!"
     else:
         value += data
-    if (value != old_value and value != '') or ('0' != old_value and value == ''):
+    if (value != value2 and value != '') or ('0' != value2 and value == ''):
         if value == '':
             await bot.edit_message_caption(chat_id=query.message.chat.id, message_id=query.message.message_id, caption='0', reply_markup=home)
         else:
             await bot.edit_message_caption(chat_id=query.message.chat.id, message_id=query.message.message_id, caption=value, reply_markup=home)
-    old_value = value
+    value2 = value
 
     if value == '⛔️ Tizimda xatolik!':
         value = ''
